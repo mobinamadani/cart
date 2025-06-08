97452a5a884d7806e22d2ee279bb24d9829bbe15
unknown <teach.madani@gmail.com.com>
2025-06-08 00:00:30 -0700

    [refac]productsTable

--- database/migrations/2025_06_07_193147_create_products_table.php
+++ database/migrations/2025_06_07_193147_create_products_table.php
@@ -14,8 +14,8 @@ return new class extends Migration
         Schema::create('products', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->json('color');
-            $table->json('size');
+            $table->json('color')->nullable();
+            $table->json('size')->nullable();
             $table->timestamps();
         });
     }
